06e2d0169438f79915ea5fd29dc2071d43334bab
Nitzan Frock <nitzanfrock@gmail.com>
2018-02-28 19:45:53 -0500

    Updates

--- src/components/Year/Year.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Year = (props) => {
-    return (
-        <div><strong>{props.year}</strong></div>
-    );
-};
-
-export default Year;
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Layout from './Layout';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Layout />, document.getElementById('root'));
-registerServiceWorker();
